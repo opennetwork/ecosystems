1d99f3fbd99c8e25075de32daeb1618ad6603839
Cloudswept <developer@cloudswept.com>
2020-05-16 14:45:42 +1200

    Offer Catalog

---
+++ schema-org-logistics/src/things/offer-catalog.ts
@@ -0,0 +1,23 @@
+// https://schema.org/OfferCatalog
+import { Thing } from "./thing"
+
+export interface OfferCatalogThing extends Thing<"OfferCatalog"> {
+
+}
+
+export interface OfferCatalogProperties {
+    itemListElement?: SchemaOrgEnvironmentThings["Offer"]
+    numberOfItems?: number
+}
+
+export interface OfferCatalog extends OfferCatalogThing, OfferCatalogProperties {
+
+}
+
+declare global {
+
+    interface SchemaOrgEnvironmentThings {
+        OfferCatalog: OfferCatalogThing | OfferCatalog
+    }
+
+}
